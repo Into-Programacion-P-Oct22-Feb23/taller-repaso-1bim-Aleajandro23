5c2bb08444437fe57707b99f8d92a35fb961d655
Ruben Condoy <rubenaleajandro@gmail.com>
2022-11-30 15:32:07 -0500

    Taller repaso

---
+++ solucion/taller08/src/taller08/Taller08.java
@@ -0,0 +1,102 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package taller08;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ *
+ * @author SALA I
+ */
+public class Taller08 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US);
+
+        String nombre;
+        int cedula;
+        double horas_m;
+        double horas_n;
+        double valor_m = 10;
+        double valor_n = 15;
+        String ciudad;
+        String mensaje = "";
+        int contador = 0;
+        boolean bandera = true;
+        int opcion ;
+        int extra = 10;
+        double sueldo_m;
+        double sueldo_n;
+        double sueldo_f=0;
+        double adicional;
+        double sueldo_s;
+        double seguro_s;
+        double seguro_s1 = 18;
+        double social;
+
+        while (bandera = true) {
+            
+            entrada.nextLine();
+            System.out.println("Ingrese nombre de empleado");
+            nombre = entrada.nextLine();
+            System.out.println("Ingrese la ciudad de la empresa");
+            ciudad = entrada.nextLine();
+            System.out.println("Ingrese el numero de cedula");
+            cedula = entrada.nextInt();
+            System.out.println("Ingrese las horas matutinas");
+            horas_m = entrada.nextDouble();
+            System.out.println("Ingrese las horas nocturnas");
+            horas_n = entrada.nextDouble();
+            
+            
+
+            sueldo_m = horas_m * valor_m;
+            sueldo_n = horas_n * valor_n;
+            sueldo_s = sueldo_m + sueldo_n;
+             
+                    
+            if (horas_n >= 10) {
+                adicional = (sueldo_m * extra) / 100;
+                sueldo_n = sueldo_n + adicional;
+                sueldo_f = sueldo_n + sueldo_m;
+                seguro_s = (sueldo_f * seguro_s1) / 100;
+                social = sueldo_f - seguro_s;
+                mensaje = mensaje + "Nomina de trabajadores: \n" + nombre + " "
+                        + cedula + "\n" + "Horas matutinas trabajadas: " + horas_m
+                        + "Horas nocturnas trabajadas: " + horas_n + "\n"
+                        + "Subtotal: " + sueldo_f + "\n" + "Seguro social: "
+                        + seguro_s + "Total a cancelar " + social;
+
+                contador = contador + 1;
+
+                
+               
+
+                }else {
+                    mensaje = mensaje + "Horas matutinas trabajadas: " + horas_m
+                            + "Horas nocturnas trabajadas: " + horas_n
+                            + "Total a cancelar: " + sueldo_f;
+                }
+            System.out.println("Desea salir del proceso? "
+                        + "Ingrese 1 para salir");
+                 
+                opcion = entrada.nextInt();
+                
+                if (opcion == 1) {
+                    bandera = false;
+            }
+        
+        }
+
+    }
+
+}
