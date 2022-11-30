ef537ee902da7644c25f7b0613c7fb1200eb4dea
Ruben Condoy <rubenaleajandro@gmail.com>
2022-11-30 15:51:02 -0500

    Repaso

--- solucion/taller08/src/taller08/Taller08.java
+++ solucion/taller08/src/taller08/Taller08.java
@@ -32,20 +32,20 @@ public class Taller08 {
         String mensaje = "";
         int contador = 0;
         boolean bandera = true;
-        int opcion ;
+        String opcion;
         int extra = 10;
         double sueldo_m;
         double sueldo_n;
-        double sueldo_f=0;
+        double sueldo_f = 0;
         double adicional;
         double sueldo_s;
         double seguro_s;
         double seguro_s1 = 18;
         double social;
 
-        while (bandera = true) {
+        while (bandera) {
+
             
-            entrada.nextLine();
             System.out.println("Ingrese nombre de empleado");
             nombre = entrada.nextLine();
             System.out.println("Ingrese la ciudad de la empresa");
@@ -56,14 +56,11 @@ public class Taller08 {
             horas_m = entrada.nextDouble();
             System.out.println("Ingrese las horas nocturnas");
             horas_n = entrada.nextDouble();
-            
-            
 
             sueldo_m = horas_m * valor_m;
             sueldo_n = horas_n * valor_n;
             sueldo_s = sueldo_m + sueldo_n;
-             
-                    
+
             if (horas_n >= 10) {
                 adicional = (sueldo_m * extra) / 100;
                 sueldo_n = sueldo_n + adicional;
@@ -76,25 +73,22 @@ public class Taller08 {
                         + "Subtotal: " + sueldo_f + "\n" + "Seguro social: "
                         + seguro_s + "Total a cancelar " + social;
 
-                contador = contador + 1;
-
-                
-               
-
-                }else {
-                    mensaje = mensaje + "Horas matutinas trabajadas: " + horas_m
-                            + "Horas nocturnas trabajadas: " + horas_n
-                            + "Total a cancelar: " + sueldo_f;
-                }
-            System.out.println("Desea salir del proceso? "
-                        + "Ingrese 1 para salir");
-                 
-                opcion = entrada.nextInt();
-                
-                if (opcion == 1) {
-                    bandera = false;
+                //contador = contador + 1;
+            } else {
+                mensaje = mensaje + "Horas matutinas trabajadas: " + horas_m
+                        + "Horas nocturnas trabajadas: " + horas_n
+                        + "Total a cancelar: " + sueldo_f;
             }
-        
+            entrada.nextLine();
+            System.out.println("Desea salir del proceso? Ingrese s para salir");
+
+            opcion = entrada.nextLine();
+
+            if (opcion.equals("s")) {
+                bandera = false;
+            }
+            System.out.println (mensaje);
+
         }
 
     }
